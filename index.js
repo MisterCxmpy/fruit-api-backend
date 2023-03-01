9aff9b96ca17a3ed39ac2ff1899c327f45c83d31
MisterCxmpy <cosgunhussein@gmail.com>
2023-03-01 15:56:01 +0000

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require("express");
 require("dotenv").config()
+
+const express = require("express");
+const cors = require("cors")
 const app = express();
 const port = process.env.PORT;
 
@@ -7,6 +9,7 @@ const fruitsHome = require("./json/fruits-home.json");
 const fruitsError = require("./json/fruits-error.json");
 const fruitsList = require("./json/fruits.json");
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
